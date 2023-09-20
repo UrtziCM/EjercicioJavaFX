490d495b2316cde5702a79e2dd70342db8dadfed
Urtzi <urcrmedev@gmail.com>
2023-09-20 13:50:00 +0200

    DOC Añadidos comentarios para mejor legibilidad.

--- src/ejerciciodesdecero/Ejercicio.java
+++ src/ejerciciodesdecero/Ejercicio.java
@@ -18,31 +18,48 @@ public class Ejercicio extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		/* Creacion de panel y escena principal */
 		BorderPane mainPane = new BorderPane();
 		mainScene = new Scene(mainPane);
+		
+		/* Creamos un boton y un textfield para que se introduzcan datos */
 		Button botonAceptar = new Button("Aceptar");
 		TextField txtfNombre = new TextField();
 
+		/* Establecemos el outcome de pulsar el boton */
 		botonAceptar.setOnAction(e -> modalSaludo(txtfNombre.getText()));
+
+		/* Centramos el boton en la pantalla */
 		BorderPane.setAlignment(botonAceptar, Pos.CENTER);
-		mainPane.setCenter(txtfNombre);
-		mainPane.setBottom(botonAceptar);
-		botonAceptar.setAlignment(Pos.CENTER);
+		/* Colocamos ambos campos en el panel principal */
+		mainPane.setCenter(txtfNombre); // Panel en el centro
+		mainPane.setBottom(botonAceptar); // Panel en la parte de abajo
+
+		/* Establecemos la escena y enseñamos la ventana */
 		stage.setScene(mainScene);
 		stage.show();
 	}
-
+	/**
+	 * Crea una ventana modal a partir del nombre dado,
+	 * en caso de que el nombre coincida con obi wan sin
+	 * tener en cuenta si es mayus. o minus. , responderá con 
+	 * 'General Kenobi' en caso contrario simplemente aparecerá: 
+	 * "Hola " + nombre (parámetro)
+	 * 
+	 * @param nombre Nombre del TextField de la ventana padre
+	 */
 	private static void modalSaludo(String nombre) {
 		Stage modal = new Stage();
-		modal.setTitle("");
+		modal.setTitle(""); // Borramos el título
 		VBox mainPane = new VBox();
 		Scene modalScene = new Scene(mainPane);
-		if (nombre.toLowerCase().equals("obi wan")) {
+		/* Si el nombre es Obi Wan o derivados, cambiamos el saludo a General Kenobi*/
+		if (nombre.toLowerCase().contains("obi wan")) {
 			mainPane.getChildren().add(new Text("General Kenobi!"));
 		} else {
 			mainPane.getChildren().add(new Text("Hola " + nombre));
 		}
-
+		/* Establecemos escena y enseñamos la ventana */
 		modal.setScene(modalScene);
 		modal.show();
 	}
