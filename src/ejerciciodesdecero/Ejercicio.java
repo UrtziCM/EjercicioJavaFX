0979f9c426e3875bcbcc424667237260c82b7844
Urtzi <urcrmedev@gmail.com>
2023-09-20 14:05:25 +0200

    UPDATE Añadido boton de cierre en la parte superior derecha, y funcion de cierre para el boton.

--- src/ejerciciodesdecero/Ejercicio.java
+++ src/ejerciciodesdecero/Ejercicio.java
@@ -21,13 +21,18 @@ public class Ejercicio extends Application {
 		BorderPane mainPane = new BorderPane();
 		mainScene = new Scene(mainPane);
 		Button botonAceptar = new Button("Aceptar");
+		Button botonCerrar = new Button("x");
 		TextField txtfNombre = new TextField();
 
 		botonAceptar.setOnAction(e -> txtfNombre.getText());
+		botonCerrar.setOnAction(e -> stage.close());
 		BorderPane.setAlignment(botonAceptar, Pos.CENTER);
 		mainPane.setCenter(txtfNombre);
 		mainPane.setBottom(botonAceptar);
+		mainPane.setTop(botonCerrar);
 		botonAceptar.setAlignment(Pos.CENTER);
+
+		BorderPane.setAlignment(botonCerrar, Pos.TOP_RIGHT);
 
 		stage.setResizable(false);
 		stage.setHeight(100);
